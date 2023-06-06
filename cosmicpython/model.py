f709e6dd1315ba7e206598215fd6f0c3790f21ed
sijinkim <tlwlsdi0306@gmail.com>
2023-06-06 15:55:04 +0900

    list 사용한 Batch 구조

--- cosmicpython/model.py
+++ cosmicpython/model.py
@@ -15,7 +15,21 @@ class Batch:
         self.sku = sku
         self.eta = eta
         self.available_quantity = qty
+        self.allocated_lines: list[str] = []
 
     def allocate(self, line: OrderLine) -> None:
-        if self.sku == line.sku:
+        if self.can_allocate(line):
             self.available_quantity -= line.qty
+            self.allocated_lines.append(line.orderid)
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return (
+            self.sku == line.sku
+            and self.available_quantity >= line.qty
+            and line.orderid not in self.allocated_lines
+        )
+
+    def deallocate(self, line: OrderLine) -> None:
+        if line.orderid in self.allocated_lines:
+            self.available_quantity += line.qty
+            self.allocated_lines.remove(line.orderid)
--- tests/test_batches.py
+++ tests/test_batches.py
@@ -3,10 +3,42 @@ from datetime import date
 from cosmicpython.model import Batch, OrderLine
 
 
-def test_allocating_to_a_batch_reduces_the_available_quantity():
-    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
-    line = OrderLine("order-ref", "SMALL-TABLE", 2)
+def make_batch_and_line(sku: str, batch_qty: int, line_qty: int):
+    return (
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty),
+    )
 
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch, line = make_batch_and_line("SMALL-TABLE", 20, 2)
     batch.allocate(line)
 
     assert batch.available_quantity == 18
+
+
+def test_can_allocate_if_available_greater_than_required():
+    large_batch, small_line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
+    assert large_batch.can_allocate(small_line)
+
+
+def test_cannot_allocate_if_available_smaller_than_required():
+    small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
+    assert small_batch.can_allocate(large_line) is False
+
+
+def test_can_allocate_if_available_equeal_to_required():
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
+    assert batch.can_allocate(line)
+
+
+def test_cannot_allocate_if_skus_do_not_match():
+    batch = Batch("batch-001", "UNCOMFORTABLE-TABLE", 100, eta=None)
+    different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
+    assert batch.can_allocate(different_sku_line) is False
+
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATED-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
