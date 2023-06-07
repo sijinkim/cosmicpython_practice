b3ddfd0c820f875cc0f4d67dc336e9912fdd8678
sijinkim <tlwlsdi0306@gmail.com>
2023-06-07 23:56:19 +0900

    값 객체와 엔티티를 코드에 명시적으로 표현

--- cosmicpython/model.py
+++ cosmicpython/model.py
@@ -35,3 +35,11 @@ class Batch:
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Batch):
+            return False
+        return self.reference == other.reference
+
+    def __hash__(self) -> int:
+        return hash(self.reference)
